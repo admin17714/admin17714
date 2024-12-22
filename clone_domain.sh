b34327c945488a41038891242f95f8ff4a32b2d9
admin17714 <File_upload_now@proton.me>
2024-12-23 01:55:54 +0530

    Add files via upload

---
+++ clone_domain.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Remotify - Clone Domain
+
+# Print Error, Delete File & Exit Process
+# ======================================================
+function process_failed() {
+    exit 1
+}
+function process_success() {
+    exit 0
+}
+# ======================================================
+
+
+# Collect & Validate Parameters
+# ======================================================
+fromDomain=$1
+toDomain=$2
+
+[ -z "$1" ] && process_failed
+[ -z "$2" ] && process_failed
+# ======================================================
+
+
+# Main Process Code
+# ======================================================
+function process_do() {
+
+    # Empty public_html Directory
+	rm -rf /home/admin/web/$toDomain/public_html/*		&> /dev/null || process_failed
+	
+    # Clone Domain
+	cp -rf /home/admin/web/$fromDomain/public_html/* /home/admin/web/$toDomain/public_html		&> /dev/null || process_failed
+}
+# ======================================================
+
+
+# Execute Main Code & Delete File
+# ======================================================
+process_do
+process_success
+# ======================================================
